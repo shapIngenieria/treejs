54754620a5906d23e7208f8442aab9d457acf75b
shapIngenieria <103613988+shapIngenieria@users.noreply.github.com>
2023-08-17 23:21:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,63 +1,28 @@
-// import * as THREE from 'three';
+import * as THREE from 'three';
 
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x005300 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x005300 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-// camera.position.z = 5;
-// renderer.render( scene, camera );
+camera.position.z = 5;
+renderer.render( scene, camera );
 
-// function animate() {
-// 	requestAnimationFrame( animate );
+function animate() {
+	requestAnimationFrame( animate );
 
-// 	cube.rotation.x += 0.005;
-// 	cube.rotation.y += 0.005;
-
-// 	renderer.render( scene, camera );
-// }
-
-// animate();
-
-
-const raycaster = new THREE.Raycaster();
-const pointer = new THREE.Vector2();
-
-function onPointerMove( event ) {
-
-	// calculate pointer position in normalized device coordinates
-	// (-1 to +1) for both components
-
-	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-}
-
-function render() {
-
-	// update the picking ray with the camera and pointer position
-	raycaster.setFromCamera( pointer, camera );
-
-	// calculate objects intersecting the picking ray
-	const intersects = raycaster.intersectObjects( scene.children );
-
-	for ( let i = 0; i < intersects.length; i ++ ) {
-
-		intersects[ i ].object.material.color.set( 0xff0000 );
-
-	}
+	cube.rotation.x += 0.005;
+	cube.rotation.y += 0.005;
 
 	renderer.render( scene, camera );
-
 }
 
-window.addEventListener( 'pointermove', onPointerMove );
+animate();
 
-window.requestAnimationFrame(render);
